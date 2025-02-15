0940976e0ac7442b21a1d10b4aba9099ed93cf5f
Nishank1705 <nishothers@gmail.com>
2025-02-15 23:51:24 +0530

    [Nishank Bathri] Add. Regex and Junit

---
+++ Day5/Junit/Advanced Problems/Java code/Problem3.java
@@ -0,0 +1,11 @@
+package org.example.JUnit_Problems.AdvanceProblems;
+public class Problem3 {
+
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9/5) + 32;
+    }
+
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5/9;
+    }
+}
